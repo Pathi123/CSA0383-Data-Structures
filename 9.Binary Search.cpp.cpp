5a1da11a1a43309015845f05616ce3ba5499d4b0
Pathi123 <110801045+Pathi123@users.noreply.github.com>
2022-10-06 11:13:11 +0530

    Add files via upload

---
+++ 9.Binary Search.cpp.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>  
+int binarySearch(int a[], int beg, int end, int val)    
+{    
+    int mid;    
+    if(end >= beg)     
+    {        mid = (beg + end)/2;    
+/* if the item to be searched is present at middle */  
+        if(a[mid] == val)    
+        {                 
+            return mid+1;    
+        }    
+            /* if the item to be searched is smaller than middle, then it can only be in left subarray */  
+        else if(a[mid] < val)     
+        {  
+            return binarySearch(a, mid+1, end, val);    
+        }    
+            /* if the item to be searched is greater than middle, then it can only be in right subarray */  
+        else     
+        {  
+            return binarySearch(a, beg, mid-1, val);    
+        }          
+    }    
+    return -1;     
+}   
+int main() {  
+  int a[100],val,n,i;
+   printf("Enter the size of array: ");
+  scanf("%d",&n);
+  printf("Enter the elements: ");
+  for(i=0;i<n;i++)
+  {
+  	scanf("%d",&a[i]);
+  }
+  printf("Enter the element to search: ");
+  scanf("%d",&val);  
+  int res = binarySearch(a, 0, n-1, val); // Store result  
+  printf("The elements of the array are - ");  
+  for (int i = 0; i < n; i++)  
+  printf("%d ", a[i]);   
+  printf("\nElement to be searched is - %d", val);  
+  if (res == -1)  
+  printf("\nElement is not present in the array");  
+  else  
+  printf("\nElement is present at %d position of array", res);  
+  return 0;  
+}  
